8b4d7228e55bde014a72ab7168ad3574731dad34
Yanan Sun <ynsun@thoughtworks.com>
2022-03-25 16:46:22 +0800

    add conv model

--- src/model.ts
+++ src/model.ts
@@ -7,3 +7,23 @@ export function createDenseModel() {
   model.add(layers.dense({ units: 10, activation: 'softmax' }))
   return model
 }
+
+export function ConvModel() {
+  const model = sequential()
+  model.add(
+    layers.conv2d({
+      inputShape: [28, 28, 1],
+      kernelSize: 3,
+      filters: 16,
+      activation: 'relu'
+    })
+  )
+
+  model.add(layers.maxPooling2d({ poolSize: 2, strides: 2 }))
+
+  model.add(layers.flatten({ inputShape: [13, 13, 16] }))
+  model.add(layers.dense({ units: 42, activation: 'relu' }))
+  model.add(layers.dense({ units: 10, activation: 'softmax' }))
+
+  return model
+}
